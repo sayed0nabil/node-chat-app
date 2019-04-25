ee811f4c9ace45cc981c94603337504f1d46e228
Seko Nabil <sayed0nabil@gmail.com>
2019-04-25 11:10:41 +0200

    Connect And Disconnect Event Handlers

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const rightPath = `${__dirname}/public`,
       http      = require('http');
 // NPM packages
 const express = require('express'),
-      app     = express();
+      app     = express(),
+      socketIO = require('socket.io');
 
 
 const port  =  process.env.PORT || 4000;
 const server = http.createServer(app);
+const io = socketIO(server);
 app.use(express.static(rightPath));
-app.get('/main', (req, res) => {
-    res.sendFile(`${rightPath}/index.html`);
-})
-app.listen(port, () => console.log(`listening on port ${port}`));
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+    socket.on('disconnect', (socket) => console.log('User Disconnected'));
+});
+// app.get('/main', (req, res) => {
+//     res.sendFile(`${rightPath}/index.html`);
+// })
+server.listen(port, () => console.log(`listening on port ${port}`));
