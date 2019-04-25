70c228ca204506997727ab1fa114e8bb50d2b357
Seko Nabil <sayed0nabil@gmail.com>
2019-04-25 09:34:29 +0200

    Adding Main Route

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ const express = require('express'),
 const port  =  process.env.PORT || 4000;
 const server = http.createServer(app);
 app.use(express.static(rightPath));
+app.get('/', (req, res) => {
+    res.sendFile(`${rightPath}\\index.html`);
+})
 app.listen(port, () => console.log(`listening on port ${port}`));
