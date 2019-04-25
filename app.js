e057283e3b12bd06c78cf2600eec69851cd05432
Seko Nabil <sayed0nabil@gmail.com>
2019-04-25 09:45:43 +0200

    Update Route File

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const rightPath = `${__dirname}\\public`,
+const rightPath = `${__dirname}/public`,
       http      = require('http');
 // NPM packages
 const express = require('express'),
@@ -8,7 +8,7 @@ const express = require('express'),
 const port  =  process.env.PORT || 4000;
 const server = http.createServer(app);
 app.use(express.static(rightPath));
-app.get('/', (req, res) => {
-    res.sendFile(`${rightPath}\\index.html`);
+app.get('/main', (req, res) => {
+    res.sendFile(`${rightPath}/index.html`);
 })
 app.listen(port, () => console.log(`listening on port ${port}`));
